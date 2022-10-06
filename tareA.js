1750e086b89d4f84b125893862cda957deb8ad2c
alejo1765 <-v>
2022-10-05 21:21:03 -0300

    Agregamos archivos js y html

---
+++ tareA.js
@@ -0,0 +1,62 @@
+//#region 
+var variable1="mañana";
+let variable2=" es jueves";
+variable2=" es jueves";
+const si="is";
+console.log (variable1+variable2)
+//#endregion
+//#region 
+const num1=2;
+const num2=4;
+function sumaNumeros(N1,N2){
+    const resultado=N1+N2;
+    return resultado
+}
+const resultado=sumaNumeros(num1,num2)
+console.log (resultado)
+//#endregion
+//#region 
+const num3=10;
+const num4=5;
+function restaNumeros(N3,N4){
+    const resultado1=N3-N4;
+    return resultado1
+}
+const resultado1=restaNumeros(num3,num4)
+console.log (resultado1)
+//#endregion
+//#region 
+const num5=2;
+const num6=4;
+function multNumeros(N5,N6){
+    const resultado2=N5*N6;
+    return resultado2
+}
+const resultado2=multNumeros(num5,num6)
+console.log (resultado2)
+//#endregion
+//#region 
+const num7=40;
+const num8=2;
+function divNumeros(N7,N8){
+    const resultado3=N7/N8;
+    return resultado3
+}
+const resultado3=divNumeros(num7,num8)
+console.log (resultado3)
+//#endregion 
+//#region
+//array
+const misAmigos =['Jorge','Lautaro','Becky G',30,2];
+console.log(misAmigos[0]+ `se dice que Lautaro no sabe jugar a la pelota pero quien sabe...
+nacio el dia ` + misAmigos[3] + " del mes " + misAmigos[4])
+  //recorrer un array con for
+for (let i = 0; i <= misAmigos.length; i++){
+    console.log(misAmigos[i]);
+}
+ //recorrer un array con for of
+ for (let m of misAmigos){
+    console.log(m);
+ }
+//#endregion
+
