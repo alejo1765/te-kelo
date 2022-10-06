3c6be3d190dcc3e4a1b0c5aef3f87fd70879bcbc
alejo1765 <-v>
2022-10-05 21:50:36 -0300

    Agrego funcion matematica (suma)

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ variable2=" es jueves";
 const si="is";
 console.log (variable1+variable2)
 //#endregion
+//#region 
+const num1=2;
+const num2=4;
+function sumaNumeros(N1,N2){
+    const resultado=N1+N2;
+    return resultado
+}
+const resultado=sumaNumeros(num1,num2)
+console.log (resultado)
+//#endregion
